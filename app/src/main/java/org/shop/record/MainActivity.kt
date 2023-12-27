c13add5108f85cce1a63adf3e4557e2e695d55b6
wbham <wbham97@naver.com>
2023-12-27 22:19:33 +0900

    [Feat] - 권한 요청 Process
    - 녹음 버튼 클릭 시 시스템 권한 요청 팝업
    - 거절 시 교육용 Dialog 띄움
    - 이 때에도 거절 시 그대로 취소. 다시 녹음 버튼 클릭하면 교육용 Dialog가 그대로 표시
    - 교육용 Dialog에서 '권한 허용하기' 클릭 시 첫 팝업과 동일한 시스템 팝업이 띄워짐
    - 하지만 이 때에도 거절한다면 최종 Setting에서 직접 설정하라는 Dialog 띄움. 확인 누르면 시스템 설정으로 이동

--- app/src/main/java/org/shop/record/MainActivity.kt
+++ app/src/main/java/org/shop/record/MainActivity.kt
@@ -1,7 +1,15 @@
 package org.shop.record
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.shop.record.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +20,102 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
+        binding.recordButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 권한이 있으면 실제로 녹음을 시작
+
+                }
+
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) -> {
+                    showPermissionRationalDialog()
+                }
+
+                else -> {
+                    // 첫 시스템 팝업
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        REQUEST_REQUEST_AUDIO_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    /**
+     *  왜 권한이 필요한지 알려주는 메서드
+     */
+    private fun showPermissionRationalDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("녹음 권한을 켜주셔야 앱을 정상적으로 사용할 수 있습니다.")
+            .setPositiveButton("권한 허용하기") { _, _ ->
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    REQUEST_REQUEST_AUDIO_CODE
+                )
+            }
+            .setNegativeButton("취소") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .show()
+    }
+
+    private fun showPermissionSettingDialog() {
+        AlertDialog.Builder(this)
+            .setMessage("녹음 권한을 켜주셔야 앱을 정상적으로 사용할 수 있습니다. 앱 설정화면으로 진입해서 권한을 켜주세요")
+            .setPositiveButton("권한 변경하러 가기") { _, _ ->
+                navigateToAppSetting()
+            }
+            .setNegativeButton("취소") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .show()
+    }
+
+    private fun navigateToAppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            // 우리 앱의 상세 설정으로 바로 이동
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_REQUEST_AUDIO_CODE && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if (audioRecordPermissionGranted) {
+            // TODO 녹음 작업을 시작
+
+        } else {
+            // 권한 팝업은 뜬적 있는데 거절했네? 그럼 이제 교육을 시키자
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                )
+            ) {
+                showPermissionRationalDialog()
+            } else {
+                showPermissionSettingDialog()
+            }
+        }
+    }
+
+    companion object {
+        private val REQUEST_REQUEST_AUDIO_CODE = 200
     }
 }
